2ffcd14cd923890a98651a9a0f3da1deca6fdde4
Ferin @ lapHP <ferinsharaf23@gmail.com>
2025-12-06 23:08:16 +0530

    Major Changes to SEO and Optimization

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import { Suspense } from 'react'
+import { useInView } from 'react-intersection-observer'
+
 import VideoBackground from './components/VideoBackground'
 import Header from './components/Header'
 import Hero from "./components/Hero"
@@ -6,7 +9,10 @@ import './App.css'
 
 import bgVideo from './assets/bg_video.mp4'
 
+// const NewContent = React.lazy(() => import ('./components/NewContent'))
+
 function App() {
+  const { ref: refNewContent, inView: inViewNewContent } = useInView({ threshold: 0.1, triggerOnce: true })
   
   return (
     <>
@@ -14,6 +20,14 @@ function App() {
       <VideoBackground videoSource={bgVideo}/>
       <Header />
       <Hero />
+      {/* New content here */}
+      <div ref = {refNewContent}>
+        {inViewNewContent && (
+          <Suspense fallback = {<div>Loading ...</div>}>
+            {/* <NewContent /> */}
+          </Suspense>
+        )}
+      </div>
     </>
   )
 }
---
+++ src/components/NewContent.tsx
@@ -0,0 +1,10 @@
+
+const NewContent = () => {
+    return (
+        <div style={{ width: "100%", boxSizing: 'border-box' }}>
+            <h1 style={{ color: 'red', margin: 0 }}>New Content</h1>
+        </div>
+    )
+}
+
+export default NewContent

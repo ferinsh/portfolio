7e23f255e32d533657cbb11e6e60b25910ba58ec
Ferin @ lapHP <ferinsharaf23@gmail.com>
2025-12-07 21:51:06 +0530

    Add project header

--- src/components/ProjectBar.tsx
+++ src/components/ProjectBar.tsx
@@ -9,8 +9,8 @@ const ProjectBar: React.FC = () => {
     const projects = [
         {projectName: "project", projectThumbnail: "/vite.svg", projectDetails: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est fugit commodi unde quisquam deleniti quibusdam maxime dolorem quod quae laborum minus expedita minima aliquid doloremque, esse qui incidunt. Hic, deserunt!Lorem ipsum dolor sit amet consectetur adipisicing elit. Est fugit commodi unde quisquam deleniti quibusdam maxime dolorem quod quae laborum minus expedita minima aliquid doloremque, esse qui incidunt. Hic, deserunt!", projectTags: ["vite"]},
         {projectName: "project", projectThumbnail: "/vite.svg", projectDetails: "Vite template", projectTags: ["Vite", "mysql"]},
-        {projectName: "project", projectThumbnail: "/vite.svg", projectDetails: "Vite template", projectTags: ["rEact", "vite"]},
-        {projectName: "project", projectThumbnail: "/vite.svg", projectDetails: "Vite template", projectTags: ["express", "react", "vite"]},
+        {projectName: "project", projectThumbnail: "/vite.svg", projectDetails: "Vite template", projectTags: ["rEactjs", "vite"]},
+        {projectName: "project", projectThumbnail: "/vite.svg", projectDetails: "Vite template", projectTags: ["express", "reactjs", "vite"]},
     ]
 
     let tags = projects.flatMap(project => project.projectTags);
@@ -18,6 +18,7 @@ const ProjectBar: React.FC = () => {
     tags = [...new Set(tags)]
 
     return (<>
+        <h1 style={{color: "white", textAlign: 'left', marginBottom: "1rem"}}>Projects</h1>
         <section className="project-bar" id="project-bar">
             <ProjectBarheader tags={tags}/>
             <ProjectBarContent projects={projects}/>

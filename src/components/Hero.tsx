6af284cbcc1381a68443cbd8de40078baa654122
Ferin @ lapHP <ferinsharaf23@gmail.com>
2025-12-08 13:15:34 +0530

    Add animation to hero name

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,8 +8,8 @@ const Hero = () => {
                     <img src="https://static.photos/monochrome/200x200/6.webp" alt="my image" />
                 </section>
                 <section className="hero-child" id="hero-details">
-                    <h1 className="hero-detail-item">Ferin Sharaf</h1>
-                    <h4 className="hero-detail-item">Full Stack Developer</h4>
+                    <h1 className="hero-detail-item" id="hero-detail-name"><p>Ferin Sharaf</p></h1>
+                    <h4 className="hero-detail-item" id="hero-detail-byline"><p>Full Stack Developer</p></h4>
                 </section>
             </section>
         </>
